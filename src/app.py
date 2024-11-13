21437011840a2b128682b36b9ca3af1f15f748f5
IsmaelMooBa <122620548+IsmaelMooBa@users.noreply.github.com>
2024-11-13 01:09:06 -0600

    Mi primer commit

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+templateDir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+templateDir = os.path.join(templateDir, 'src', 'templates')
+
+app = Flask(__name__, template_folder = templateDir)
+
+#Routers
+@app.route('/')
+def gome():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
